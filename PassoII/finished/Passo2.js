8b2a43b149cd71f3cf0e5d6291d48559046451aa
Eduardo Braz do Vale <edu.bvale@gmail.com>
2012-11-09 00:42:09 -0200

    Criado startPoint do Passo II

---
+++ PassoII/finished/Passo2.js
@@ -0,0 +1,52 @@
+
+var Passo2Layer = cc.Layer.extend({   
+    ctor:function(){
+    	
+    	
+    	//Adicionando Label
+    	var label = cc.LabelTTF.create( "Cocos2D ☺ Html5", "Helvetica", 64 );
+       
+        label.setPosition( cc.p( 300, 450 ) );
+
+        this.addChild(label);
+
+        //Adicionando Sprite
+        var sprite = cc.Sprite.create("../../img/carcara.png");
+
+        sprite.setPosition( cc.p( 300, 250 ) );
+
+        this.addChild(sprite);
+
+        //Manipulando o Sprite
+        sprite.schedule(function(){
+
+	        this.setRotation( this.getRotation() + 3);
+        });
+    }
+});
+
+Passo2Scene = cc.Scene.extend({
+	onEnter:function(){
+		
+		this._super();
+
+
+		var lazyLayer = new cc.Layer.create();
+
+		this.addChild(lazyLayer);
+
+		var backgroundSprite = cc.Sprite.create("../../img/bg.png");
+
+		backgroundSprite.setPosition(cc.p(300,250));
+
+		lazyLayer.addChild(backgroundSprite, 0);
+
+
+		var layer = new Passo2Layer();
+
+		layer.init();
+
+		this.addChild(layer);
+	}
+})
+
